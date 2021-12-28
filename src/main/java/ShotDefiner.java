aabe6c5df820342935ef3548d8e8ee471876f79c
Pedro Gomes <pedromgomes29@gmail.com>
2021-12-28 18:41:14 +0000

    Added draw to Shot Interface

--- src/main/java/ShotDefiner.java
+++ src/main/java/ShotDefiner.java
@@ -1,7 +1,12 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.io.IOException;
+
 public interface ShotDefiner {
     Integer getDamage();
     Integer getWidth();
     Integer getVelocity();
     Position getPosition();
     void moveDown();
+    void draw(TextGraphics graphics) throws IOException;
 }
