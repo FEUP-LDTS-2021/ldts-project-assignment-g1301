129912beb83a064058ed73aa123a893e591f4874
Pedro Gomes <pedromgomes29@gmail.com>
2021-12-30 18:52:30 +0000

    Added Arena Inteface

---
+++ src/main/java/ArenaDefiner.java
@@ -0,0 +1,38 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ArenaDefiner {
+
+    List<Enemy> createEnemies();
+
+    @Override
+    ArenaDefiner();
+
+    void addSpell();
+
+    void setSpaceship(Spaceship s);
+
+    Integer getLevel();
+
+    List<Enemy> getEnemies();
+    List<SpellTemplate> getSpells();
+    Spaceship getSpaceship();
+    Integer getWidth();
+    Integer getHeight();
+    void draw(TextGraphics graphics) throws IOException;
+    void checkShotCollisions();
+    boolean checkShotsHitSpaceship();
+
+    void processKey(KeyStroke key);
+
+    void checkShotsHitEnemies();
+
+    void removeShotsOutOfBounds();
+
+    void moveEnemies();
+
+    boolean moveShots();
+}
