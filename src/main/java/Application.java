2f16ac1400aa04885fb99955a141908f2e684479
rui-exe <mrswager173@gmail.com>
2022-01-17 18:54:07 +0000

    Fixed bug on application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         try{
-            Game g = Game.getInstance();
-            g.run();
             Menu m = new Menu();
             m.interactions();
         }
