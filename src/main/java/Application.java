c1167371602a34d87760dd7a9ff0f667dcd7dd12
rui-exe <mrswager173@gmail.com>
2022-01-17 18:25:15 +0000

    Created Class Menu that passes the test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         try{
-            Game g = new Game();
-            g.run();
+            Menu m = new Menu();
+            m.interactions();
         }
         catch(IOException e){
             e.printStackTrace();;
